3f4c72249d58e3d44efde16b09a46c2c8996f8bb
kipk49 <kiplk2@illinois.edu>
2019-07-03 11:59:47 -0500

    started work on python script to convert .csv files into pyplot graph

---
+++ Python Scripts/csv_to_graph.py
@@ -0,0 +1,12 @@
+#import csv
+#from matplotlib import pyplot as plt
+
+#file = input("Enter the full csv file name")
+
+#print("Hello World")
+
+#with open()
+
+f = open("hello.txt", "w+")
+f.write("Hello World")
+f.close()
